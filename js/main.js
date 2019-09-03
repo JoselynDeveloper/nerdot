3a5c25aa2a02c70d5a8b88f41bca166eb733f5e7
ghatiko <ghatiko_chemako@hotmail.com>
2019-09-03 12:20:00 -0400

    tercera actualizacion

--- js/main.js
+++ js/main.js
@@ -5,6 +5,16 @@ $(document).ready(function () {
         $(".iconSlider2").addClass("carouselSinFondo");
         $(".iconSlider3").addClass("carouselSinFondo");
         $(".iconSlider4").addClass("carouselSinFondo");
+
+        //FUNCIONAMIENTO PARA EL CAMBIO DE LAS IMAGENES PRIMER SLIDER
+        $(".slider2a").addClass("d-none");
+        $(".slider2b").removeClass("d-none");
+
+        $(".slider1a").addClass("d-none");
+        $(".slider1b").removeClass("d-none");
+
+        $(".slider3a").addClass("d-none");
+        $(".slider3b").removeClass("d-none");
     });
 
     $(".iconSlider2").click(function () {
@@ -12,6 +22,16 @@ $(document).ready(function () {
         $(".iconSlider1").addClass("carouselSinFondo");
         $(".iconSlider3").addClass("carouselSinFondo");
         $(".iconSlider4").addClass("carouselSinFondo");
+
+        //FUNCIONAMIENTO PARA EL CAMBIO DE LAS IMAGENES PRIMER SLIDER
+        $(".slider2a").removeClass("d-none");
+        $(".slider2b").addClass("d-none");
+
+        $(".slider1a").addClass("d-none");
+        $(".slider1b").removeClass("d-none");
+
+        $(".slider3b").addClass("d-none");
+        $(".slider3a").removeClass("d-none");
     });
 
     $(".iconSlider3").click(function () {
@@ -19,6 +39,17 @@ $(document).ready(function () {
         $(".iconSlider2").addClass("carouselSinFondo");
         $(".iconSlider1").addClass("carouselSinFondo");
         $(".iconSlider4").addClass("carouselSinFondo");
+
+        //FUNCIONAMIENTO PARA EL CAMBIO DE LAS IMAGENES PRIMER SLIDER
+        $(".slider2a").addClass("d-none");
+        $(".slider2b").removeClass("d-none");
+
+        $(".slider1b").addClass("d-none");
+        $(".slider1a").removeClass("d-none");
+
+        $(".slider3b").addClass("d-none");
+        $(".slider3a").removeClass("d-none");
+        
     });
 
     $(".iconSlider4").click(function () {
@@ -27,6 +58,12 @@ $(document).ready(function () {
         $(".iconSlider3").addClass("carouselSinFondo");
         $(".iconSlider1").addClass("carouselSinFondo");
     });
+
+    $(".dropdown-menu").css('margin-top', 0);
+    $(".dropdown").mouseover(function () {
+        $(this).addClass('show').attr('aria-expanded', "true");
+        $(this).find('.dropdown-menu').addClass('show');
+    })
 
 
 });
