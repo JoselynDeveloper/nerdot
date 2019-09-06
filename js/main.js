99d77c7dd628815595d1d297b30aedb827a2d962
ghatiko <ghatiko_chemako@hotmail.com>
2019-09-06 10:47:36 -0400

    sexta actualizacion

--- js/main.js
+++ js/main.js
@@ -15,6 +15,9 @@ $(document).ready(function () {
 
         $(".slider3a").addClass("d-none");
         $(".slider3b").removeClass("d-none");
+
+        // QUITAR EL MOUSE ROJO
+        $(".mouserojo").addClass("d-none");
     });
 
     $(".iconSlider2").click(function () {
@@ -23,7 +26,7 @@ $(document).ready(function () {
         $(".iconSlider3").addClass("carouselSinFondo");
         $(".iconSlider4").addClass("carouselSinFondo");
 
-        //FUNCIONAMIENTO PARA EL CAMBIO DE LAS IMAGENES PRIMER SLIDER
+        //FUNCIONAMIENTO PARA EL CAMBIO DE LAS IMAGENES SEGUNDO SLIDER
         $(".slider2a").removeClass("d-none");
         $(".slider2b").addClass("d-none");
 
@@ -32,6 +35,9 @@ $(document).ready(function () {
 
         $(".slider3b").addClass("d-none");
         $(".slider3a").removeClass("d-none");
+
+        // PONER EL MOUSE ROJO
+        $(".mouserojo").removeClass("d-none");
     });
 
     $(".iconSlider3").click(function () {
@@ -40,7 +46,7 @@ $(document).ready(function () {
         $(".iconSlider1").addClass("carouselSinFondo");
         $(".iconSlider4").addClass("carouselSinFondo");
 
-        //FUNCIONAMIENTO PARA EL CAMBIO DE LAS IMAGENES PRIMER SLIDER
+        //FUNCIONAMIENTO PARA EL CAMBIO DE LAS IMAGENES TERCER SLIDER
         $(".slider2a").addClass("d-none");
         $(".slider2b").removeClass("d-none");
 
@@ -49,21 +55,21 @@ $(document).ready(function () {
 
         $(".slider3b").addClass("d-none");
         $(".slider3a").removeClass("d-none");
-        
+
+        // QUITAR EL MOUSE ROJO
+        $(".mouserojo").addClass("d-none");
     });
 
-    $(".iconSlider4").click(function () {
-        $(".iconSlider4").removeClass("carouselSinFondo");
-        $(".iconSlider2").addClass("carouselSinFondo");
-        $(".iconSlider3").addClass("carouselSinFondo");
-        $(".iconSlider1").addClass("carouselSinFondo");
-    });
 
     $(".dropdown-menu").css('margin-top', 0);
     $(".dropdown").mouseover(function () {
         $(this).addClass('show').attr('aria-expanded', "true");
-        $(this).find('.dropdown-menu').addClass('show');
+        $(this).find('.dropdown-menu').show();
     })
+
+    $(".dropdown-menu").mouseleave(function() {;
+        $(this).hide(1000);
+    });
 
 
 });
